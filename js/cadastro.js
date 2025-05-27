5d1e89adb255a4751af53be147e6a065b6bd2ec4
JoaoChoma <jchomax@gmail.com>
2025-05-26 21:07:27 -0300

    editar e remover (editar nao finalizado)

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,10 +1,11 @@
 // cadastro com crud
 
 const botao = document.getElementById('btnCadastrar');
-const listaUsuario = [];
+//const listaUsuario = [];
 
 // criacao registro
 botao.addEventListener('click', function () {
+    listaUsuario = JSON.parse(localStorage.getItem("usuarios")) || [];
     const usuario = {
         usuario: document.getElementById('login').value,
         senha: document.getElementById('senha').value
@@ -13,6 +14,8 @@ botao.addEventListener('click', function () {
     listaUsuario.push(usuario);
     listaJson = JSON.stringify(listaUsuario);
     localStorage.setItem("usuarios", listaJson);
+    document.getElementById('login').value = '';
+    document.getElementById('senha').value = '';
     listar();
 });
 
@@ -33,6 +36,27 @@ function listar() {
         `;
         tabela.appendChild(linha);
     });
+}
+
+function editarUsuario(index){
+    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+    //usuariosCadastrados[3];
+    const objUsuario = usuariosCadastrados[index];
+    document.getElementById("login").value = objUsuario.usuario;
+    document.getElementById("senha").value = objUsuario.senha;
+
+}
+
+function excluirUsuario(index){
+    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    if(confirm("Voce realmente quer excluir?")){
+        usuariosCadastrados.splice(index, 1);
+        listaJson = JSON.stringify(usuariosCadastrados);
+        localStorage.setItem("usuarios", listaJson);
+    }
+
+    listar();
 }
 
 listar();
